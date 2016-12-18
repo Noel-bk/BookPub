d4c6360f65d78f79e4a8932e68c0eb536d749b1a
Noel.b <jaeyeong.b@gmail.com>
2016-12-18 23:22:23 +0900

    Add Scheduling

--- src/main/java/org/test/bookpub/StartupRunner.java
+++ src/main/java/org/test/bookpub/StartupRunner.java
@@ -4,6 +4,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.test.bookpub.repository.BookRepository;
 
 /**
@@ -16,7 +17,12 @@ public class StartupRunner implements CommandLineRunner {
     @Autowired
     private BookRepository bookRepository;
 
-    @Override
+    @Scheduled(initialDelay = 1000, fixedRate = 10000)
+    public void run() {
+        logger.info("Number of books: " + bookRepository.count());
+    }
+
+    @Overrid
     public void run(String... args) throws Exception {
         logger.info("Number of books: " + bookRepository.count());
     }
