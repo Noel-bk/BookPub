e32cda238aea0b2bb78f484591b19f959431a412
Noel.b <jaeyeong.b@gmail.com>
2016-12-21 23:13:05 +0900

    Add some test data

--- src/main/java/org/test/bookpub/StartupRunner.java
+++ src/main/java/org/test/bookpub/StartupRunner.java
@@ -5,7 +5,12 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.test.bookpub.entity.Author;
+import org.test.bookpub.entity.Book;
+import org.test.bookpub.entity.Publisher;
+import org.test.bookpub.repository.AuthorRepository;
 import org.test.bookpub.repository.BookRepository;
+import org.test.bookpub.repository.PublisherRepository;
 
 /**
  * Created by Noel on 12/17/16.
@@ -17,6 +22,12 @@ public class StartupRunner implements CommandLineRunner {
     @Autowired
     private BookRepository bookRepository;
 
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    @Autowired
+    private PublisherRepository publisherRepository;
+
     @Scheduled(initialDelay = 1000, fixedRate = 10000)
     public void run() {
         logger.info("Number of books: " + bookRepository.count());
@@ -24,6 +35,15 @@ public class StartupRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
+        // Add some test data
+        Author author = new Author("Alex", "Antonov");
+        author = authorRepository.save(author);
+        Publisher publisher = new Publisher("Packt");
+        publisher = publisherRepository.save(publisher);
+        Book book = new Book("978-1-78528-415-1", "Spring Boot Recipes", author, publisher);
+        bookRepository.save(book);
+
         logger.info("Number of books: " + bookRepository.count());
     }
 }
