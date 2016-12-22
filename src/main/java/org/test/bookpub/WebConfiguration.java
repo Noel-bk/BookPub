fe11171cbbe9813578c7b7c4e0cef4334017a606
Noel.b <jaeyeong.b@gmail.com>
2016-12-22 23:27:41 +0900

    Add custom connectors

--- src/main/java/org/test/bookpub/WebConfiguration.java
+++ src/main/java/org/test/bookpub/WebConfiguration.java
@@ -1,10 +1,18 @@
 package org.test.bookpub;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.catalina.connector.Connector;
 import org.apache.catalina.filters.RemoteIpFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -15,12 +23,15 @@ import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.test.bookpub.formatters.BookFormatter;
 import org.test.bookpub.repository.BookRepository;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Noel on 12/19/16.
  */
 @Configuration
+@PropertySource("classpath:/tomcat.https.properties")
+@EnableConfigurationProperties(WebConfiguration.TomcatSslConnectorProperties.class)
 public class WebConfiguration extends WebMvcConfigurerAdapter {
 
     @Autowired
@@ -66,6 +77,46 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/internal/**")
             .addResourceLocations("classpath:/");
+    }
+
+    @Getter
+    @Setter
+    @ConfigurationProperties(prefix = "custom.tomcat.https")
+    public static class TomcatSslConnectorProperties {
+        private Integer port;
+        private Boolean ssl = true;
+        private Boolean secure = true;
+        private String scheme = "https";
+        private File keystore;
+        private String keystorePassword;
+
+        public void configureConnector(Connector connector) {
+            if (port != null)
+                connector.setPort(port);
+            if (secure != null)
+                connector.setSecure(secure);
+            if (scheme != null)
+                connector.setScheme(scheme);
+            if (ssl != null)
+                connector.setProperty("SSLEnabled", ssl.toString());
+            if (keystore != null && keystore.exists()) {
+                connector.setProperty("keystoreFile", keystore.getAbsolutePath());
+                connector.setProperty("keystorePassword", keystorePassword);
+            }
+        }
+    }
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer(TomcatSslConnectorProperties properties) {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+        tomcat.addAdditionalTomcatConnectors(createSslConnector(properties));
+        return tomcat;
+    }
+
+    private Connector createSslConnector(TomcatSslConnectorProperties properties) {
+        Connector connector = new Connector();
+        properties.configureConnector(connector);
+        return connector;
     }
 
 }
